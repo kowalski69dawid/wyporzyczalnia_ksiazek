fd2db95e6156ff196ecd2922c9e3e61a07591e64
unknown <programista171@gmail.com>
2019-03-20 10:26:37 +0100

    Dodanie tabulatorów w main.js; poprawienie działania logowania i rejestracji

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 ﻿$(document).ready(function(){
-$("#logowanieForm").hide();
-$("#rejestracjaForm").hide();
-$("#logowanie").click(logowanieForm);
-$("#rejestracja").click(rejestracjaForm);
-$(document).on("click", "#zwinLogowanie", function(){
-$("#logowanie").html("Zaloguj się");
-});
-$(document).on("click", "#zwinRejestracja", function(){
-$("#rejestracja").html("Zarejestruj się");
-});
-$(document).on("click", "#logowanieSubmit", logowanie);
-$(document).on("click", "#rejestracjaSubmit", rejestracja);
+	$("#logowanieForm").hide();
+	$("#rejestracjaForm").hide();
+	$("#logowanie").click(logowanieForm);
+	$("#rejestracja").click(rejestracjaForm);
+	$(document).on("click", "#zwinLogowanie", function(){
+		$("#logowanieForm").hide();
+		$("#logowanie").show();
+	});
+	$(document).on("click", "#zwinRejestracja", function(){
+		$("#rejestracjaForm").hide();
+		$("#rejestracja").show();
+	});
+	$(document).on("click", "#logowanieSubmit", logowanie);
+	$(document).on("click", "#rejestracjaSubmit", rejestracja);
 });//document function
